cdb3e55678a4e563edde742acf3637e63cc9b045
H B <phajib@gmail.com>
2019-01-23 08:04:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|str| str.start_with?("r", "R")}
+end
+
+def contain_a(array)
+  array.select {|str| str.include?("a")}
+end
+
+def first_wa(array)
+  array.find {|str| str[0..1] == "wa"}
+end
+
+def remove_non_strings(array)
+  array.delete_if {|str| str.class != String}
+end
+
+def count_elements(array)
+  array.uniq.each do |i|
+    count = 0
+    array.each do |i2|
+      if i2 == i
+        count += 1
+      end
+    end
+    i[:count] = count
+  end
+end
+
+def merge_data(keys, data)
+
+end
+
+def find_cool(array)
+
+end
+
+def organize_schools(array)
+
+end
