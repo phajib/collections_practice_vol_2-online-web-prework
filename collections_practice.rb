b2711b2c8c6b9746802520f58ae4ad77322c3470
H B <phajib@gmail.com>
2019-01-23 10:44:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,7 +42,7 @@ end
 
 def organize_schools(schools)
   new_schools = {}
-  schools.map do |school, location|
+  schools.collect do |school, location|
     location.map do |k, v|
       unless new_schools.key?(v)
         new_schools[v] = [school]
